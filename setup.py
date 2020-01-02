7aa96e81858d7a2d22ec673c240325e35dba94df
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-01-02 13:03:02 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ from pathlib import Path
 setup(
     name='forbiddenfluent',
     version='0.1',
+    url="https://github.com/nickdelgrosso/forbiddenfluent",
     author="Nicholas A. Del Grosso",
     author_email="delgrosso.nick@gmail.com",
     description="Adds iteration methods like zip, filter, apply to built-in Python collections in a fluent programming style.",
-    long_description=Path("README.md").read_text()
+    #long_description="This module adds the built-in functions that work on iterables and make them into methods in order to support a fluent programming style.",
     packages=find_packages(),
     license='GNU General Public License, Version 3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        ],
+    python_requires='>=3.6',
 )
